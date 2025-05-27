61fd872b655625ff087b7788f7ffe7204b0d8493
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-05-27 07:52:29 +0530

    deployment

--- calc-be/constants.py
+++ calc-be/constants.py
@@ -6,4 +6,8 @@ SERVER_URL = 'localhost'
 PORT = '8900'
 ENV = 'dev'
 
+SERVER_URL = os.getenv("SERVER_URL", "0.0.0.0")
+PORT = os.getenv("PORT", "8000")
+ENV = os.getenv("ENV", "prod")
+
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
